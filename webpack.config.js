3354009c6ce8bf7e9139806628b2e95324d1da42
Clint Bush <clint.bush@homesite.com>
2017-02-01 13:11:53 -0800

    React-Boilerplate v1.0

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                tests: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
