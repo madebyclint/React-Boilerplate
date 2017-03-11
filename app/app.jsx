af41dd67e4cd6f8250bfbfcf99eae7ba3e935283
Clint Bush <clint@madebyclint.com>
2017-03-11 10:51:55 -0800

    Revise Boilerplate to include standards and ES6 linting plus yarn.

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,13 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Route, Router, IndexRoute, hashHistory} from 'react-router'
+
+// import jquery just for foundation
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery
 
 // Load Foundation
-$(document).foundation();
+$(document).foundation()
 
 // App CSS
 // Fix required http://stackoverflow.com/questions/40201262/webpack-css-syntaxerror-unexpected-token
@@ -12,4 +16,4 @@ require('applicationStyles')
 ReactDOM.render(
     <p>React Boilerplate 3 Project</p>,
     document.getElementById('app')
-);
+)
